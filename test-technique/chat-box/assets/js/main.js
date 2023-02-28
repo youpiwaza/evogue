548ad471b745ef863ffc12aca98f3268409992ce
Maxime Chevasson / Masamune <masamune.code@gmail.com>
2023-02-28 14:18:13 +0100

    🔧MOD: TT > JS > Affichage d'un message

--- test-technique/chat-box/assets/js/main.js
+++ test-technique/chat-box/assets/js/main.js
@@ -75,14 +75,62 @@ console.log(conversations);
 //* Ajouter un message
 //      A quel chat auquel rajouter le message
 //      Contenu du message
-//      Est-il propriétaire ?
+//          Est-il propriétaire ?
 //      Masquer "Début de la conversation"
 //      Mettre à jour le temps depuis le dernier message reçu
-conversations[0].messages.innerHTML = "lel";
+function ajouterMessage( chat, contenu, isProprietaire ) {
+
+    // On masque le début de la conversation
+    chat.texteDuDebut.classList.add('hidden');
+
+    // On rajoute le message à la suite des autres
+    // chat.messages.innerHTML = genererContenuDuMessage( contenu, isProprietaire);
+    chat.messages.innerHTML += genererContenuDuMessage( contenu, isProprietaire );
+}
+ajouterMessage( conversations[1], 'lel', true );
+ajouterMessage( conversations[1], 'lel', false );
+ajouterMessage( conversations[1], 'Un autre test :3', true );
+
+
+
+// Générer le message encapsulé dans le html qui va bieng
+//      Vu que l'on a déjà généré le html, et ajusté le css, on copie colle simplement
+//      Puis on remplace le contenu dynamique par les variables
+function genererContenuDuMessage( contenu, isProprietaire ) {
+    
+    // return isProprietaire ? yep : nope;
+
+    if(isProprietaire) {
+        return `
+            <div class="chat-bubble chat-bubble-right position-relative">
+
+                <div class="chat-bubble-actions position-absolute top-0 end-0 me-1 mt-1">
+                    <button class="btn-display btn btn-light">
+                        <i class="fa-solid fa-pen-to-square"></i>
+                    </button>
+                    <button class="btn-display btn btn-light">
+                        <i class="fa-solid fa-trash"></i>
+                    </button>
+                </div>
+                
+                <span class="chat-bubble-content">${ contenu }</span>
+            </div>
+        `;
+    }
+    else {
+        return `
+            <div class="chat-bubble disabled">
+                <span class="chat-bubble-content">${ contenu }</span>
+            </div>
+        `;
+    }
+}
+
 
 // * Gestion du formulaire
 //      Envoi lors du clic sur le bouton
-//      Réutiliser les données fournies
+//          Réutiliser les données fournies
+//          Envoi aux deux chats
 //      Vider le champ texte
 
 // * Réponses suggérées
